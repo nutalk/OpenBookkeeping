658de5b4c18ed57b3d7e7a2715f07ed80ef2c74b
Tao He <tao.he@mixosense.com>
2023-07-16 15:25:52 +0800

    Adjust main window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QGridLayout, QTableWidget,\
-    QTableWidgetItem, QMenuBar, QMenu, QFileDialog
+    QTableWidgetItem, QMenuBar, QMenu, QFileDialog, QStatusBar, QLabel
 from PySide6.QtGui import QAction
 from faker import Faker
 from pathlib import Path
@@ -19,6 +19,7 @@ class MyWindow(QMainWindow):
 
         self.resize(900, 700)
         self.setWindowTitle('财记')
+
         self.new_file_action = QAction('新建账本')
         self.openFile = QAction('打开账本')
         self.fileMenu = QMenu('文件')
@@ -27,7 +28,6 @@ class MyWindow(QMainWindow):
         self.menuBar().addMenu(self.fileMenu)
 
         self.propAction = QAction('新增资产')
-
         self.liabilityAction = QAction('新增负债')
         self.propMenu = QMenu('新增账户')
         self.propMenu.addAction(self.propAction)
@@ -36,10 +36,24 @@ class MyWindow(QMainWindow):
         self.tables = MainTables()
         self.setCentralWidget(self.tables)
 
-        self.new_prop_widget = NewProp()
-        self.new_liability_widget = NewLiability()
+        self.new_prop_widget = None
+        self.new_liability_widget = None
 
         self.band()
+        self.update_status()
+
+    def update_status(self):
+        self.status_bar = QStatusBar()
+        self.status_info = QLabel(f'账本文件: {self.database}')
+        self.status_bar.addWidget(self.status_info)
+        self.setStatusBar(self.status_bar)
+
+        if self.database is None:
+            self.propAction.setDisabled(True)
+            self.liabilityAction.setDisabled(True)
+        else:
+            self.propAction.setDisabled(False)
+            self.liabilityAction.setDisabled(False)
 
     def band(self):
         self.propAction.triggered.connect(self.new_prop_fuc)
@@ -52,17 +66,23 @@ class MyWindow(QMainWindow):
         file_use = file_dialog.getSaveFileName(self, "新建文件", filter=".bp (*.bk)")
         init_db(file_use[0])
         self.database = file_use[0]
+        self.update_status()
 
     def open_db_fuc(self):
         file_dialog = QFileDialog(self)
         file_use = file_dialog.getOpenFileName(self, "选择文件", filter=".bp (*.bk)")
         file_path = Path(file_use[0])
         self.database = file_path
+        self.update_status()
 
     def new_prop_fuc(self):
+        if self.new_prop_widget is None:
+            self.new_prop_widget = NewProp(self.database)
         self.new_prop_widget.show()
 
     def new_liability_fuc(self):
+        if self.new_liability_widget is None:
+            self.new_liability_widget = NewLiability(self.database)
         self.new_liability_widget.show()
 
 
