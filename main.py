258126b2c79c4c639160ba227541d3805b91de61
tao.he <tao.he@mixosense.com>
2023-07-30 14:46:57 +0800

    Adjust update after waper

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QHBoxLayout, QGridLayout, QTableWidget,\
     QTableWidgetItem, QMenuBar, QMenu, QFileDialog, QStatusBar, QLabel
 from PySide6.QtGui import QAction
-
+from functools import wraps
 from pathlib import Path
 
 from OpenBookkeeping.main_window_center import MainLayouts
@@ -12,7 +12,6 @@ class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.database = None
-
         self.resize(1080, 700)
         self.setWindowTitle('财记')
 
@@ -37,24 +36,52 @@ class MyWindow(QMainWindow):
 
         self.new_prop_widget = None
         self.new_liability_widget = None
+        self.band()
 
+    @property
+    def ready(self):
+        if self.database is not None and Path(self.database).exists():
+            return True
+        else:
+            return False
+
+    def update_content(self):
+        if self.ready:
+            self.propMenu.setEnabled(True)
+            self.check_action.setEnabled(True)
+            self.pred_action.setEnabled(True)
+        else:
+            self.propMenu.setEnabled(False)
+            self.check_action.setEnabled(False)
+            self.pred_action.setEnabled(False)
+
+    def update_after(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            res = func(self, *args, **kwargs)
+            self.update_content()
+            return res
+
+        return wrapper
+
+    @update_after
+    def band(self):
+        self.openFile.triggered.connect(self.open_db_fuc)
+        self.new_file_action.triggered.connect(self.new_db_fuc)
+
+    @update_after
     def new_db_fuc(self):
         file_dialog = QFileDialog(self)
         file_use = file_dialog.getSaveFileName(self, "新建文件", filter=".bk (*.bk)")
         init_db(file_use[0])
         self.database = file_use[0]
 
+    @update_after
     def open_db_fuc(self):
         file_dialog = QFileDialog(self)
         file_use = file_dialog.getOpenFileName(self, "选择文件", filter=".bk (*.bk)")
         file_path = file_use[0]
         self.database = file_path
-
-    def new_prop_fuc(self):
-        self.new_prop_widget.show()
-
-    def new_liability_fuc(self):
-        self.new_liability_widget.show()
 
 
 if __name__ == "__main__":
