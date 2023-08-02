33659fd372caced9059dabd4b0805632b941b6cf
tao.he <tao.he@mixosense.com>
2023-08-02 22:07:49 +0800

    Adjust detail table header

--- OpenBookkeeping/fuc.py
+++ OpenBookkeeping/fuc.py
@@ -1,3 +1,5 @@
+from PySide6.QtCore import QDate, Signal, QAbstractTableModel, Qt
+from loguru import logger
 
 
 def trans_data(data: list) -> dict:
@@ -11,3 +13,36 @@ def trans_data(data: list) -> dict:
             data_rec[type_id]['recs'].append(rec)
     return data_rec
 
+
+class DetailTableModel(QAbstractTableModel):
+
+    def __init__(self):
+        super().__init__()
+        self._data = []
+        self._header = []
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            return self._data[index.row()][index.column()]
+
+    def rowCount(self, index):
+        # The length of the outer list.
+        return len(self._data)
+
+    def columnCount(self, index):
+        # The following takes the first sub-list, and returns
+        # the length (only works if all rows are an equal length)
+        if len(self._data) == 0:
+            return 0
+        else:
+            return len(self._data[0])
+
+    def update_content(self, data: list, header: list):
+        self._data = data
+        self._header = header
+
+    def headerData(self, section, orientation, role):
+        if role != Qt.DisplayRole:
+            return None
+        if orientation == Qt.Horizontal:
+            return self._header[section]
--- OpenBookkeeping/main_window_detail.py
+++ OpenBookkeeping/main_window_detail.py
@@ -1,7 +1,7 @@
 from PySide6.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, \
     QHBoxLayout, QVBoxLayout, QGridLayout, QDoubleSpinBox, QSpinBox, QComboBox, \
     QTextEdit, QDateEdit, QListView, QSpacerItem, QSizePolicy, QDialog, QMessageBox, \
-    QGroupBox, QTableView
+    QGroupBox, QTableView, QHeaderView
 from PySide6.QtCore import QDate, Signal, QAbstractTableModel, Qt
 from loguru import logger
 from functools import wraps
@@ -9,32 +9,7 @@ from PySide6.QtGui import QStandardItemModel, QStandardItem, QColor
 
 from OpenBookkeeping.new_prop import PropList
 from OpenBookkeeping.sql_db import query_by_col
-
-
-class DetailTableModel(QAbstractTableModel):
-
-    def __init__(self):
-        super().__init__()
-        self._data = []
-
-    def data(self, index, role):
-        if role == Qt.DisplayRole:
-            return self._data[index.row()][index.column()]
-
-    def rowCount(self, index):
-        # The length of the outer list.
-        return len(self._data)
-
-    def columnCount(self, index):
-        # The following takes the first sub-list, and returns
-        # the length (only works if all rows are an equal length)
-        if len(self._data) == 0:
-            return 0
-        else:
-            return len(self._data[0])
-
-    def update_content(self, data: list):
-        self._data = data
+from OpenBookkeeping.fuc import DetailTableModel
 
 
 class DetailTable(QWidget):
@@ -43,14 +18,16 @@ class DetailTable(QWidget):
         right_layout = QVBoxLayout()
         right_layout.addWidget(QLabel('账户变化明细'))
         self.detail_table = QTableView(self)
+        self.detail_table.setEditTriggers(QTableView.EditTrigger.NoEditTriggers)
+        self.detail_table.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeMode.Stretch)
         self.detail_model = DetailTableModel()
         self.detail_table.setModel(self.detail_model)
         right_layout.addWidget(self.detail_table)
         self.setLayout(right_layout)
 
-    def update_content(self, data: list):
-        logger.debug(f'{data=}')
-        self.detail_model.update_content(data)
+    def update_content(self, data: list, header: list):
+        logger.debug(f'{data=}, {header=}')
+        self.detail_model.update_content(data, header)
         # 通知viewer
         self.detail_model.layoutChanged.emit()
 
@@ -82,7 +59,14 @@ class DetailPage(QWidget):
         if len(info) == 1:
             prop_id = info[0][0]
             details = query_by_col(self.database, 'prop_details', 'target_id', prop_id)
-            self.detail.update_content(details)
+            headers = ['id', '账户名称', '交易日期', '交易金额', '备注', '余额']
+            output = []
+            sum = 0
+            for row in details:
+                sum += row[3]
+                new_row = [row[0], prop_name] + list(row[2:]) + [sum]
+                output.append(new_row)
+            self.detail.update_content(output, header=headers)
         else:
             logger.error(f'length of prop invalid {info}')
 
