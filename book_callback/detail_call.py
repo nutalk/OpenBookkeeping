d0bb9c1fa0b5d14ecc023d9c37e6e177f4719a7e
Tao He <tao.he@mixosense.com>
2024-03-04 17:14:13 +0800

    Remove form and moda

--- book_callback/detail_call.py
+++ book_callback/detail_call.py
@@ -10,13 +10,11 @@ from config import db_path
 from OpenBookkeeping.sql_db import (query_table, add_prop, query_by_col,
                                     update_by_col, del_by_col)
 from OpenBookkeeping.gloab_info import account_info_show, prop_type_items, liability_currency_types
-from book_callback.detail_fuc import get_prop_list, get_prop_form
 
 
 # 更新账户基本信息
 @callback(
-    [Output(f'account_info_{_}', 'children') for _ in account_info_show] +
-    [Output('current_prop_info', 'data')],
+    [Output(f'account_info_{_}', 'children') for _ in account_info_show],
     [Input({"type": "prop_list_item", "index": ALL}, 'n_clicks')]
 )
 def update_account_page(n_clicks):
@@ -39,7 +37,6 @@ def update_account_page(n_clicks):
         else:
             content = rec_content
         output.append(content)
-    output.append({'id': btn_id['index'], 'name': rec[1]})
 
     return tuple(output)
 
@@ -78,63 +75,3 @@ def update_account_detail_table(n_clicks):
     return table
 
 
-#删除账户的对话框
-@callback(
-    [Output('del_prop_modal', 'is_open'),
-     Output('del_prop_confirm_txt', 'children'),
-     Output('to_update_prop_table', 'data', allow_duplicate=True)],
-    [Input('prop_del_btn','n_clicks'),
-     Input('del_prop_save', 'n_clicks'),
-     Input('del_prop_cancel', 'n_clicks'),
-     Input('current_prop_info', 'data')],
-    prevent_initial_call=True
-)
-def show_del_prop_modal(del_btn, del_save, del_cancel, prop_info):
-    btn_id = ctx.triggered_id
-    to_update = {'to_update': False}
-    if btn_id == 'prop_del_btn':
-        return True, f"确定删除 {prop_info['name']} 吗？", to_update
-    elif btn_id == 'del_prop_save':
-        del_by_col(db_path, 'prop', 'id', prop_info['id'])
-        return False, '', {'to_update': True}
-    elif btn_id == 'del_prop_cancel':
-        return False, '', to_update
-    else:
-        raise PreventUpdate
-
-
-#编辑，新增，删除账户之后，更新左侧的列表
-@callback(
-    Output('detail_prop_list', 'children'),
-    Input('to_update_prop_table', 'data')
-)
-def update_prop_list(to_update):
-    if to_update['to_update']:
-        return get_prop_list(db_path)
-    else:
-        raise PreventUpdate
-
-#TODO 编辑账户信息
-@callback(
-    [Output('edit_prop_modal', 'is_open'),
-     Output('edit_prop_body', 'children'),
-     Output('to_update_prop_table', 'data', allow_duplicate=True)],
-    [Input('prop_edit_btn','n_clicks'),
-     Input('edit_prop_save', 'n_clicks'),
-     Input('edit_prop_cancel', 'n_clicks'),
-    Input('current_prop_info', 'data')],
-    prevent_initial_call=True
-)
-def show_edit_prop_modal(edit_btn, edit_save, edit_cancel, prop_info):
-    btn_id = ctx.triggered_id
-    to_update = {'to_update': False}
-    if btn_id == 'prop_edit_btn':
-        form_output = get_prop_form()
-        return True, form_output, to_update
-    elif btn_id == 'edit_prop_save':
-        del_by_col(db_path, 'prop', 'id', prop_info['id'])
-        return False, '', {'to_update': True}
-    elif btn_id == 'edit_prop_cancel':
-        return False, '', to_update
-    else:
-        raise PreventUpdate
--- book_layout/detail_page.py
+++ book_layout/detail_page.py
@@ -1,7 +1,7 @@
 import dash_bootstrap_components as dbc
 from dash import html, dcc
 import pandas as pd
-from book_callback.detail_fuc import get_prop_list, get_prop_form
+from book_callback.detail_fuc import get_prop_list
 from config import db_path
 from OpenBookkeeping.gloab_info import account_info_show
 
@@ -12,30 +12,6 @@ account_list = dbc.Stack([
     html.H4('账户列表'),
     html.Hr(),
     html.Div(prop_list, id='detail_prop_list'),
-    dbc.Modal(
-        [
-            dbc.ModalHeader(dbc.ModalTitle('编辑账户')),
-            dbc.ModalBody(get_prop_form(), id='edit_prop_body'),
-            dbc.ModalFooter([
-                dbc.Button('保存', id='edit_prop_save'),
-                dbc.Button('取消', id='edit_prop_cancel')]
-            )
-        ], id='edit_prop_modal', size='lg', is_open=False
-    ),
-    dcc.Store(id='current_prop_info', storage_type='session', data={}),
-    dcc.Store(id='to_update_prop_table', storage_type='session', data={}),
-    dbc.Modal(
-        [
-            dbc.ModalHeader(dbc.ModalTitle('删除账户')),
-            dbc.ModalBody([
-                html.Div('确定删除该账户吗？', id='del_prop_confirm_txt'),
-            ]),
-            dbc.ModalFooter([
-                dbc.Button('确定', id='del_prop_save'),
-                dbc.Button('取消', id='del_prop_cancel')]
-            )
-        ], id='del_prop_modal', is_open=False
-    )
 ]
 )
 
