14b297c8ea9107c96140d30a5530a3ed1ef101e5
Tao He <tao.he@mixosense.com>
2024-08-07 18:11:45 +0800

    Adjust prop edit form

--- accounts/forms.py
+++ accounts/forms.py
@@ -44,6 +44,41 @@ class PropNewForm(forms.Form):
         self.helper.add_input(Submit('submit', '提交'))
 
 
+class PropEditForm(forms.Form):
+    id = forms.IntegerField(label='id')
+    name = forms.CharField(label='账户名称', max_length=255, required=True)
+    p_type = forms.ChoiceField(choices=[(idx, item) for idx, item in enumerate(prop_type_items)], 
+                                  required=True, label='账户类型')
+    start_date = forms.DateField(required=True, label='开始日期',
+                                 widget=forms.widgets.DateInput(attrs={'type': 'date'}))
+    term_month = forms.IntegerField(required=True, initial=0, label='期数')
+    rate = forms.FloatField(required=True, initial=0.0, label='年利率')
+    currency = forms.IntegerField(required=True, initial=0, label='现金流')
+    ctype = forms.ChoiceField(choices=[(idx, item) for idx, item in enumerate(liability_currency_types)], 
+                              required=True, label='还款方式')
+    comment = forms.CharField(label='备注', max_length=255, required=False)
+
+    def __init__(self, action_str, form_id: str, form_class: str, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = form_id
+        self.helper.form_class = form_class
+        self.helper.form_method = 'post'
+        self.helper.form_action = action_str
+        self.helper.layout = Layout(
+            Field('id', type='hidden'),
+            'name',
+            'p_type',
+            'start_date',
+            AppendedText('term_month', '月', active=False),
+            AppendedText('rate', '%', active=False),
+            'currency',
+            'ctype',
+            'comment'
+        )
+        self.helper.add_input(Submit('submit', '提交'))
+
+
 class DetailForm(forms.Form):
     id = forms.IntegerField(label='id')
     target_id = forms.ImageField(label='tid')
