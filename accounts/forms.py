ea3c3ab111ebb85ac2689efdd4f769e17b7bc078
nutalk <ht2005112@hotmail.com>
2024-12-20 15:08:01 +0800

    Adjust form en

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,24 +2,24 @@ from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.bootstrap import AppendedText
 from crispy_forms.layout import Submit, Layout, Field
-from .gloab_info import prop_type_ids, prop_type_items, \
-    liability_currency_ids, liability_currency_types, account_info_show
+from .gloab_info import prop_type_items, liability_currency_types
+from django.utils.translation import gettext_lazy as lazy
 
 
 class PropNewForm(forms.Form):
     id = forms.IntegerField(label='id')
-    name = forms.CharField(label='账户名称', max_length=255, required=True)
+    name = forms.CharField(label=lazy('Account Name'), max_length=255, required=True)
     p_type = forms.ChoiceField(choices=[(idx, item) for idx, item in enumerate(prop_type_items)], 
-                                  required=True, label='账户类型')
-    start_date = forms.DateField(required=True, label='开始日期',
+                                  required=True, label=lazy('Account Type'))
+    start_date = forms.DateField(required=True, label=lazy('Start Date'),
                                  widget=forms.widgets.DateInput(attrs={'type': 'date'}))
-    term_month = forms.IntegerField(required=True, initial=0, label='期数')
-    rate = forms.FloatField(required=True, initial=0.0, label='年利率')
-    currency = forms.IntegerField(required=True, initial=0, label='现金流')
+    term_month = forms.IntegerField(required=True, initial=0, label=lazy('Terms'))
+    rate = forms.FloatField(required=True, initial=0.0, label=lazy('Interest Rate'))
+    currency = forms.IntegerField(required=True, initial=0, label=lazy('Cash Flow'))
     ctype = forms.ChoiceField(choices=[(idx, item) for idx, item in enumerate(liability_currency_types)], 
-                              required=True, label='还款方式')
-    comment = forms.CharField(label='备注', max_length=255, required=False)
-    init_ammount = forms.IntegerField(required=True, initial=0, label='初始金额')
+                              required=True, label=lazy('Repayment'))
+    comment = forms.CharField(label=lazy('Comment'), max_length=255, required=False)
+    init_ammount = forms.IntegerField(required=True, initial=0, label=lazy('Init Balance'))
 
     def __init__(self, action_str, form_id: str, form_class: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -33,30 +33,30 @@ class PropNewForm(forms.Form):
             'name',
             'p_type',
             'start_date',
-            AppendedText('term_month', '月', active=False),
+            AppendedText('term_month', lazy('month'), active=False),
             AppendedText('rate', '%', active=False),
             'currency',
             'ctype',
             'comment',
-            AppendedText('init_ammount', '元', active=False)
+            AppendedText('init_ammount', lazy('$'), active=False)
         )
 
-        self.helper.add_input(Submit('submit', '提交'))
+        self.helper.add_input(Submit('submit', lazy('Submit')))
 
 
 class PropEditForm(forms.Form):
     id = forms.IntegerField(label='id')
-    name = forms.CharField(label='账户名称', max_length=255, required=True)
+    name = forms.CharField(label=lazy('Account Name'), max_length=255, required=True)
     p_type = forms.ChoiceField(choices=[(idx, item) for idx, item in enumerate(prop_type_items)], 
-                                  required=True, label='账户类型')
-    start_date = forms.DateField(required=True, label='开始日期',
+                                  required=True, label=lazy('Account Type'))
+    start_date = forms.DateField(required=True, label=lazy('Start Date'),
                                  widget=forms.widgets.DateInput(attrs={'type': 'date'}))
-    term_month = forms.IntegerField(required=True, initial=0, label='期数')
-    rate = forms.FloatField(required=True, initial=0.0, label='年利率')
-    currency = forms.IntegerField(required=True, initial=0, label='现金流')
+    term_month = forms.IntegerField(required=True, initial=0, label=lazy('Terms'))
+    rate = forms.FloatField(required=True, initial=0.0, label=lazy('Interest Rate'))
+    currency = forms.IntegerField(required=True, initial=0, label=lazy('Cash Flow'))
     ctype = forms.ChoiceField(choices=[(idx, item) for idx, item in enumerate(liability_currency_types)], 
-                              required=True, label='还款方式')
-    comment = forms.CharField(label='备注', max_length=255, required=False)
+                              required=True, label=lazy('Repayment'))
+    comment = forms.CharField(label=lazy('Comment'), max_length=255, required=False)
 
     def __init__(self, action_str, form_id: str, form_class: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -70,22 +70,22 @@ class PropEditForm(forms.Form):
             'name',
             'p_type',
             'start_date',
-            AppendedText('term_month', '月', active=False),
+            AppendedText('term_month', lazy('month'), active=False),
             AppendedText('rate', '%', active=False),
             'currency',
             'ctype',
             'comment'
         )
-        self.helper.add_input(Submit('submit', '提交'))
+        self.helper.add_input(Submit('submit', lazy('Submit')))
 
 
 class DetailForm(forms.Form):
     id = forms.IntegerField(label='id')
     target_id = forms.ImageField(label='tid')
-    occur_date = forms.DateField(required=True, label='日期',
+    occur_date = forms.DateField(required=True, label=lazy('Date'),
                                  widget=forms.widgets.DateInput(attrs={'type': 'date'}))
-    amount = forms.IntegerField(label='金额', required=True, initial=0)
-    comment = forms.CharField(label='备注', max_length=255, required=False)
+    amount = forms.IntegerField(label=lazy('amount'), required=True, initial=0)
+    comment = forms.CharField(label=lazy('Comment'), max_length=255, required=False)
 
     def __init__(self, action_str, form_id: str, form_class: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -98,9 +98,9 @@ class DetailForm(forms.Form):
             Field('id', type='hidden'),
             Field('target_id', type='hidden'),
             'occur_date',
-            AppendedText('amount', '元', active=False),
+            AppendedText('amount', lazy('$'), active=False),
             'comment'
         )
 
-        self.helper.add_input(Submit('submit', '提交'))
+        self.helper.add_input(Submit('submit', lazy('Submit')))
 
--- bookkeep/settings/settings.py
+++ bookkeep/settings/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'bookkeep.urls'
